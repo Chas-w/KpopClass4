003b8ec3e461973848a30c933dfc5137284ebc7d
Chas-w <75660257+Chas-w@users.noreply.github.com>
2023-10-13 16:39:42 -0400

    Code Cleanup, Barrier Collision Polish NEXT: Light Attack, Camera Boarders, Scene Transitions

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,20 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerControl : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
-    public float speed = 5f;
-    public float castDist = 1f;
-    public float jumpPower = 2f;
-    public float gravScale = 5f;
-    public float gravFall = 40f;
+    [Header("Input")]
+    [SerializeField] KeyCode right;
+    [SerializeField] KeyCode left;
+    [SerializeField] KeyCode up;
+    [SerializeField] KeyCode lightAttack;
+    [SerializeField] KeyCode heavyAttack;
 
+    [Header("Body")]
+    [SerializeField] Transform middle;
+
+    [Header("World")]
+    [SerializeField] float speed = 5f;
+    [SerializeField] float castDist = 1f;
+    [SerializeField] float jumpPower = 2f;
+    [SerializeField] float gravScale = 5f;
+    [SerializeField] float gravFall = 40f;
+
+    [Header("Score Management")]
+    public float health;
+    public float wins;
+    public bool dead;
+
+    #region private vars
     float horizontalMove;
 
     bool grounded = false;
     bool jump = false;
+    bool stopMoveLeft;
+    bool stopMoveRight; 
 
     Rigidbody2D myBody;
+    #endregion 
 
     void Start()
     {
@@ -24,16 +44,28 @@ public class playerControl : MonoBehaviour
 
     void Update() //use update for things that need an instant response
     {
-        horizontalMove = Input.GetAxis("Horizontal");
+        #region input
+        if (Input.GetKey(left) && !stopMoveLeft)
+        {
+            horizontalMove = -1; 
+        } else if (Input.GetKey(right) && !stopMoveRight)
+        {
+            horizontalMove = 1; 
+        } else
+        {
+            horizontalMove = 0; 
+        }
 
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetKeyDown(up) && grounded && (!stopMoveLeft || !stopMoveRight))
         {
             jump = true;
         }
+        #endregion
     }
 
     void FixedUpdate() //use fixed update for things that shouldn't fluxuate 
     {
+        #region movement
         float moveSpeed = horizontalMove * speed;
 
         #region jump
@@ -53,9 +85,9 @@ public class playerControl : MonoBehaviour
             myBody.gravityScale = gravFall; //fall with this gravity scale
         }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDist); //holds the information from a raycast hit 
+        RaycastHit2D hit = Physics2D.Raycast(middle.position, Vector2.down, castDist); //holds the information from a raycast hit 
 
-        Debug.DrawRay(transform.position, Vector2.down * castDist, Color.blue);
+        Debug.DrawRay(middle.position, Vector2.down * castDist, Color.blue);
 
         if (hit.collider != null && hit.transform.tag == "Ground")
         {
@@ -69,5 +101,33 @@ public class playerControl : MonoBehaviour
         #endregion 
 
         myBody.velocity = new Vector3(moveSpeed, myBody.velocity.y, 0f);
+        #endregion
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        #region barrierEnter
+        if (collision.gameObject.tag == "BarrierLeft")
+        {
+            stopMoveLeft = true;
+        }
+        if (collision.gameObject.tag == "BarrierRight")
+        {
+            stopMoveRight = true;
+        }
+        #endregion
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        #region barrierExit
+        if (collision.gameObject.tag == "BarrierLeft")
+        {
+            stopMoveLeft = false;
+        }
+        if (collision.gameObject.tag == "BarrierRight")
+        {
+            stopMoveRight = false;
+        }
+        #endregion
     }
 }
